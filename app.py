0fe7f43b1416667ebd0c552cab3f60e1e8719339
Brent <brent.encarnado98@gmail.com>
2023-10-20 19:03:46 -0400

    student rest api

--- app.py
+++ app.py
@@ -1,111 +1,80 @@
-# Import required libraries
 from flask import Flask, jsonify, request
 
-# Sample data: a list of tasks
-tasks = [
+students = [
     {
         'id': 1,
-        'title': 'Learn Flask',
-        'done': False
+        'name': 'Brent',
+        'grade': 100
     },
     {
         'id': 2,
-        'title': 'Develop REST API',
-        'done': False
+        'name': 'Jack',
+        'grade': 75
     }
 ]
 
-# Create a Flask application instance
 app = Flask(__name__)
 
-# Route for the root of the application
 @app.route('/')
 def hello():
-    # Just returns a welcome message
-    return "Hello. Task API is Live!"
+    return "Working"
 
-# Endpoint to get all tasks
-@app.route('/tasks', methods=['GET'])
-def get_tasks():
-    # Convert the tasks data to JSON format and return it
-    return jsonify({'tasks': tasks})
+@app.route('/students', methods=['GET'])
+def get_students():
+    return jsonify({'students': students})
 
-# Endpoint to get a specific task by its ID
-@app.route('/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    # Initialize the task variable as None
-    task = None
-    # Loop through the tasks list
-    for t in tasks:
-        if t["id"] == task_id:  # If the task with the given ID is found
-            task = t
+@app.route('/students/<int:student_id>', methods=['GET'])
+def get_student(student_id):
+    student = None
+    for t in students:
+        if t["id"] == student_id: 
+            student = t
             break
-    # If task is not found after looping
-    if task is None:
-        return jsonify({'error': 'Task not found'}), 404
-    # Return the found task
-    return jsonify({'task': task})
+    if student is None:
+        return jsonify({'error': 'student not found'}), 404
+    return jsonify({'student': student})
 
-# Endpoint to add a new task
-@app.route('/tasks', methods=['POST'])
-def add_task():
-    # Parse the 'done' status from request, default to False if it's not provided
-    status = request.json.get('done', False)
-    # Convert potential string value "true" to boolean True
-    status = status.lower() == "true"
+@app.route('/students', methods=['POST'])
+def add_student():
 
-    # Create a new task object
-    new_task = {
-        'id': len(tasks) + 1,  # Set ID as the next number in sequence
-        'title': request.json['title'],  # Get the title from request data
-        'done': status  # Set the parsed status
+    score = request.json.get('grade', False)
+
+    new_student = {
+        'id': len(students) + 1, 
+        'name': request.json['name'],  
+        'grade': score  
     }
 
-    # Add the new task to the tasks list
-    tasks.append(new_task)
+    students.append(new_student)
 
-    # Return the added task with a status code indicating resource creation
-    return jsonify({'task': new_task}), 201
+    return jsonify({'student': new_student}), 201
 
-# Endpoint to update an existing task
-@app.route('/tasks/<int:task_id>', methods=['PUT'])
-def update_task(task_id):
-    # Initialize the task variable as None
-    task = None
-    # Loop through the tasks list
-    for t in tasks:
-        if t["id"] == task_id:  # If the task with the given ID is found
-            task = t
+@app.route('/students/<int:student_id>', methods=['PUT'])
+def update_student(student_id):
+    student = None
+    for t in students:
+        if t["id"] == student_id: 
             break
-    # If task is not found after looping
-    if task is None:
-        return jsonify({'error': 'Task not found'}), 404
-    # Update task attributes with data from the request, use existing value if not provided
-    task['title'] = request.json.get('title', task['title'])
-    task['done'] = request.json.get('done', task['done'])
-    # Return the updated task
-    return jsonify({'task': task})
+    
+    if student is None:
+        return jsonify({'error': 'student not found'}), 404
+    student['name'] = request.json.get('name', student['name'])
+    student['grade'] = request.json.get('grade', student['grade'])
+    return jsonify({'student': student})
 
-# Endpoint to delete a task
-@app.route('/tasks/<int:task_id>', methods=['DELETE'])
-def delete_task(task_id):
-    global tasks  # Reference the global tasks list
-    # Initialize the task variable as None
-    task = None
-    # Loop through the tasks list
-    for t in tasks:
-        if t["id"] == task_id:  # If the task with the given ID is found
-            task = t
+@app.route('/students/<int:student_id>', methods=['DELETE'])
+def delete_student(student_id):
+    global students 
+    student = None
+    for t in students:
+        if t["id"] == student_id: 
+            student = t
             break
-    # If task is not found after looping
-    if task is None:
-        return jsonify({'error': 'Task not found'}), 404
-    # Remove the found task from the tasks list
-    tasks.remove(task)
-    # Return a success message
-    return jsonify({'result': 'Task deleted successfully'}), 200
+    if student is None:
+        return jsonify({'error': 'student not found'}), 404
+    students.remove(student)
+    return jsonify({'result': 'student deleted successfully'}), 200
 
-# This is the entry point of the application
 if __name__ == '__main__':
     # Start the Flask development server with debugging enabled
     app.run(debug=True)
